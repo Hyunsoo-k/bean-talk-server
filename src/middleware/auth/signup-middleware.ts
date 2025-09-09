a276f1b1b9efe9d878a7ebba60cdaafb2d424856
Hyunsoo_kim <red_panda119@naver.com>
2025-09-10 03:17:35 +0900

    feat : 회원가입 성공시, 회원정보 응답 x

--- src/middleware/auth/signup-middleware.ts
+++ src/middleware/auth/signup-middleware.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt";
 
 import UserModel from "../../model/user.js";
+import NotificationModel from "../../model/notification.js";
 
 const signupMiddleware = async(
   req: Request,
@@ -11,8 +12,9 @@ const signupMiddleware = async(
   const { password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = await UserModel.create({ ...req.body, password: hashedPassword });
+  const { _id: newUser_id } = newUser;
+  await NotificationModel.create({ user_id: newUser_id });
   
-  res.locals.newUser = newUser;
   next();
 };
 
