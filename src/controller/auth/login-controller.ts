f6047d92e40b6f269db1cae0b0665cc343f3e66a
Hyunsoo_kim <red_panda119@naver.com>
2025-09-09 17:45:35 +0900

    feat : 로그인, 회원가입 미들웨어, 컨트롤러 구현

---
+++ src/controller/auth/login-controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+
+const loginController = async (req: Request, res: Response): Promise<any> => {
+  const { accessToken } = res.locals;
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: false,
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
+  return res.status(200).json();
+};
+
+export default loginController;
