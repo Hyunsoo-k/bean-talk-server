05d7644e86c179c363b1b14f2d29511b649cea46
Hyunsoo_kim <red_panda119@naver.com>
2025-09-10 03:16:53 +0900

    feat : 유저 알림 라우트, 미들웨어 구현

---
+++ src/controller/user/check-user-notifications-controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+const checkUserNotificationController = async(
+  req: Request,
+  res: Response,
+): Promise<any> => {
+  res.status(201).json();
+};
+
+export default checkUserNotificationController;
---
+++ src/middleware/user/check-user-notifications-middleware.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+import customHttpErrorHandler from "../../error-handler/custom-http-error-handler.js";
+import NotificationModel from "../../model/notification.js";
+
+dotenv.config();
+
+const checkUserNotificationMiddleware = async(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  const { accessToken } = req.cookies;
+  if (!accessToken) {
+    return customHttpErrorHandler("권한이 없습니다.", 401, next);
+  }
+
+  const payload =
+    jwt.verify(accessToken, process.env.JWT_SECRET_KEY!) as { user_id: string };
+  const { user_id } = payload;
+
+  const { notification_id } = req.params;
+  await NotificationModel.findOneAndUpdate(
+    {
+      user: user_id,
+      "list._id": notification_id
+    },
+    { $set: { "list.$.isChecked": true } }
+  );
+
+  next();
+};
+
+export default checkUserNotificationMiddleware;
