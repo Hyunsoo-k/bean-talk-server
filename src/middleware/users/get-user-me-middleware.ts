86bd0e36b26efc82d30fa8178a644a360dafd528
Hyunsoo_kim <red_panda119@naver.com>
2025-09-22 03:52:29 +0900

    feat : 내 정보 요청 구현

---
+++ src/middleware/users/get-user-me-middleware.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+
+import customHttpErrorHandler from "../../error-handler/custom-http-error-handler.js";
+import verifyAccessToken from "../../utils/verify-access-token.js";
+import UserModel from "../../mongoose-model/user-model.js";
+
+const getUserMeMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+  const { accessToken } = req.cookies;
+  if (!accessToken) {
+    return customHttpErrorHandler("권한이 없습니다.", 401, next);
+  }
+
+  const { user_id } = verifyAccessToken(accessToken);
+
+  const userMe = await UserModel.findById(user_id).lean();
+   if (!userMe) {
+    return customHttpErrorHandler("유저를 찾을 수 없습니다.", 404, next);
+  }
+  const { _id, email, nickname, profileImageUrl } = userMe;
+
+  res.locals.userMe = {_id, email, nickname, profileImageUrl };
+  next();
+};
+
+export default getUserMeMiddleware;
