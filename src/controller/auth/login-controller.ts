1084445a5754a0ed6f9ab70a93bd2e107f443119
Hyunsoo_kim <red_panda119@naver.com>
2025-09-24 02:55:09 +0900

    hotfix : 개발환경 문제로 setCookie 대신 응답body에 토큰 삽입

--- src/controller/auth/login-controller.ts
+++ src/controller/auth/login-controller.ts
@@ -3,12 +3,12 @@ import { Request, Response } from "express";
 const loginController = async (req: Request, res: Response): Promise<any> => {
   const { accessToken } = res.locals;
   
-  res.cookie("accessToken", accessToken, {
-    httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000,
-  });
+  // res.cookie("accessToken", accessToken, {
+  //   httpOnly: true,
+  //   maxAge: 24 * 60 * 60 * 1000,
+  // });
 
-  return res.status(200).json();
+  return res.status(200).json(accessToken);
 };
 
 export default loginController;
