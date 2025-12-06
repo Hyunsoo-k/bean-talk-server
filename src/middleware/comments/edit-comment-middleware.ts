be0c0b62209e84d1a15eb59f58b8927d5f9abd92
Hyunsoo_kim <red_panda119@naver.com>
2025-12-07 06:49:34 +0900

    findById에 객체 형식 전달 오류 수정

--- src/middleware/comments/edit-comment-middleware.ts
+++ src/middleware/comments/edit-comment-middleware.ts
@@ -21,7 +21,7 @@ const editCommentMiddleware = async (
     throw new HttpError(400, "잘못된 카테고리 입니다.");
   }
 
-  const post = await postModelMap[category].findById({ post_id });
+  const post = await postModelMap[category].findById(post_id);
   if (!post) {
     throw new HttpError(404, "게시글을 찾을 수 없습니다.");
   }
